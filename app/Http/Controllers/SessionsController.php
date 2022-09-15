ccc3f86e77af3331b3e46ed4aade3590ee6913f7
Jon Cabot <jfc0053@gmail.com>
2022-09-15 11:09:47 -0500

    created login view, route, and controller method

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
     public function create()
     {
         return view('sessions.create');
+    }
+
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email'=> 'required|email',
+            'password'=> 'required'
+        ]);
+
+        if(auth()->attempt($attributes)) {
+            //auth fails
+            throw ValidationException::withMessages([
+                'email'=> 'Your provided credentials could not be verified.'
+            ]);
+        }
+
+        //otherwise, auth passes to HAPPY path
+        session()->regenerate();
+
+        return redirect('/')->with('success', 'You have logged in.');
     }
 
 
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,12 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 //take in Wildcard param from URI,
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('register', [RegisterController::class, 'create'])->middleware('guest'); //logic that runs when a request comes in
-
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest'); //guest = does not have to be authenticated to have access to this route
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
-Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
-Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');  //this route 'gets' the login view
+Route::post('login', [SessionsController::class, 'store'])->middleware('guest');  //this route 'posts' to the db a new session
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth'); //auth = they have to have completed auth process to have access to this route
 
 
 
